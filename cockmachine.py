1a07c3c1b9099f46ba99f98546c0bf3c60c4ecf0
rudolfg21 <rudigasperl@gmail.com>
2024-11-22 00:09:00 +0100

    bissi Cockfight

--- cockmachine.py
+++ cockmachine.py
@@ -3,10 +3,10 @@ import random
 def cockfight(player1, player2):
     winner = random.randint(1,2)
     if winner == 1:
-        player1.score = player1.score+1
+        player1.score = player1.score+10
         player2.score = player2.score-1
     else:
-        player2.score = player2.score+1
+        player2.score = player2.score+10
         player1.score = player1.score-1
 
 
