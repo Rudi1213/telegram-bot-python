ec8e1f2be08345a050071d0df096a0b5f6956176
Rudi1213 <142418254+Rudi1213@users.noreply.github.com>
2024-11-21 15:15:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ load_dotenv()
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'hello', 'random'])
+@bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Hello! I'm a simple Telegram bot.")
-
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text + " Julian leckt keine Eier :)")
 
 @bot.message_handler(commands=['random'])
 def provide_random(message):
     number = random.randInt(1,10)
     bot.reply_to(message,"Fancy random number generator: " + number)
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text + " Julian leckt keine Eier :)")
+
+
 
 
 bot.polling()
