6c57df2729600574b80b825d9df722f00b14016a
Rudi121333 <rudigasperl@gmail.com>
2025-03-10 18:02:47 +0100

    Player class is now serializable into json

--- player.py
+++ player.py
@@ -1,6 +1,17 @@
+import json
+
 class Player:
 
     def __init__(self, user_id, name):
         self.score = 10
         self.user_id = user_id
         self.name = name
+
+    def to_dict(self):
+        return {"user_id": self.user_id, "name": self.name, "score": self.score}
+
+    @classmethod
+    def from_dict(cls, data):
+        player = cls(data["user_id"], data["name"])
+        player.score = data["score"]
+        return player
