53808a9c694a75df5b384c7977a59a4b5b830ffe
Rudi121333 <rudigasperl@gmail.com>
2025-03-15 13:06:05 +0100

    fixed condition for isArbeitslosenGamerHours

--- timeManagement.py
+++ timeManagement.py
@@ -4,7 +4,7 @@ import pytz
 def isArbeitslosenGamerHours():
     vienna_tz = pytz.timezone('Europe/Vienna')
     vienna_time = datetime.now(vienna_tz)
-    if vienna_time.hour < 14 and vienna_time.weekday() in [5,6]:
+    if vienna_time.hour < 14 and vienna_time.weekday() not in [5,6]:
         return True
     else:
         return False
